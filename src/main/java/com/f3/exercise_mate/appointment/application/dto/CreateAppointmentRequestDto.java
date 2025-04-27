e8a706d52c42f7edfa710c791386a7f14abe1853
minju <betaa0528@gmail.com>
2025-04-27 22:03:42 +0900

    Feat : 약속을 생성, 수정할때의 dto들

---
+++ src/main/java/com/f3/exercise_mate/appointment/application/dto/CreateAppointmentRequestDto.java
@@ -0,0 +1,14 @@
+package com.f3.exercise_mate.appointment.application.dto;
+
+import com.f3.exercise_mate.appointment.domain.DateInfo;
+import com.f3.exercise_mate.appointment.domain.Location;
+import com.f3.exercise_mate.appointment.domain.Sport;
+
+public record CreateAppointmentRequestDto(
+        String title,
+        Long creatorId,
+        String description,
+        Sport sport,
+        Location location,
+        DateInfo dateInfo) {
+}
