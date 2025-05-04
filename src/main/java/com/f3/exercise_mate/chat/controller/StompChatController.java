029d0b113b87c353542e674317349cee9d0d1e6a
ingyeom <wjd1836@gmail.com>
2025-05-04 22:20:29 +0900

    Refactor: 스프링에서 stomp 메시지 처리의 기본 prefix로 수정

--- src/main/java/com/f3/exercise_mate/chat/controller/StompChatController.java
+++ src/main/java/com/f3/exercise_mate/chat/controller/StompChatController.java
@@ -2,27 +2,30 @@ package com.f3.exercise_mate.chat.controller;
 
 import com.f3.exercise_mate.chat.domain.ChatMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class StompChatController {
 
     private final SimpMessagingTemplate template;   // 특정 Broker로 메세지를 전달
 
-    // Client가 send 할 수 있는 경로
     // stompConfig에서 설정한 setApplicationDestinationPrefixes의 경로와 @MessageMapping 경로가 병합됨
-    // "/pub/chat/enter"
+    // /app/chat/enter
     @MessageMapping(value = "/chat/enter")
     public void enter(ChatMessage message) {
         message.setMessage(message.getWriter() + "님이 채팅방에 참여하였습니다.");
-        template.convertAndSend("/sub/chat/room/" + message.getChatRoomId(), message);
+        template.convertAndSend("/topic/chat/room/" + message.getChatRoomId(), message);
     }
 
+    // /app/chat/message
     @MessageMapping(value = "/chat/message")
     public void message(ChatMessage message) {
-        template.convertAndSend("/sub/chat/room/" + message.getChatRoomId(), message);
+        template.convertAndSend("/topic/chat/room/" + message.getChatRoomId(), message);
+        log.info(message.getMessage());
     }
 }
