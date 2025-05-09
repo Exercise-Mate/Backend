f1eceabaab5f54500bd0a88c55e63ebea64c3b73
DaehoGo <rualneox@dgu.ac.kr>
2025-05-09 21:56:56 +0900

    feat: 클럽 운동 능력 검증 로직 구현

--- src/main/java/com/f3/exercise_mate/common/entity/SportAbility.java
+++ src/main/java/com/f3/exercise_mate/common/entity/SportAbility.java
@@ -1,6 +1,7 @@
 package com.f3.exercise_mate.common.entity;
 
 import com.f3.exercise_mate.appointment.domain.Level;
+import com.f3.exercise_mate.club.domain.Club;
 
 public enum SportAbility {
     BEGINNER("입문"),
@@ -16,6 +17,10 @@ public enum SportAbility {
 
     public String getLabel() {
         return label;
+    }
+
+    public boolean isInRange(Club club) {
+        return this.ordinal() >= club.getSportAbilityMin().ordinal() && this.ordinal() <= club.getSportAbilityMax().ordinal();
     }
 
     public boolean isHigherThan(Level level) {
